a5ff39d06af8eee20c333d6f0d2beda9f07fba85
Laurent Bernaille <laurent.bernaille@gmail.com>
2016-09-15 15:38:51 +0200

    Use fmt for indentation. Use native lists in variables and outputs

--- common/variables.tf
+++ common/variables.tf
@@ -1,10 +1,47 @@
-variable  "region"                    { default = "eu-west-1" }
-variable  "application"               { default = "saltstack" }
-variable  "owner"                     { default = "d2si" }
+variable "region" {
+  type    = "string"
+  default = "eu-west-1"
+}
 
-variable  "azs"                       { default { "eu-west-1"  = "eu-west-1a,eu-west-1b,eu-west-1c"} }
-variable  "cidr_block"                { default = "10.242.0.0/16" }
-variable  "subnet_bits"               { default = "8" }
-variable  "subnet_prv_offset"         { default = "8" }
-variable  "vpc_name"                  { default = "Salt" }
-variable  "vpc_short_name"            { default = "Salt" }
+variable "application" {
+  type    = "string"
+  default = "saltstack"
+}
+
+variable "owner" {
+  type    = "string"
+  default = "d2si"
+}
+
+variable "azs" {
+  type = "map"
+
+  default {
+    "eu-west-1" = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  }
+}
+
+variable "cidr_block" {
+  type    = "string"
+  default = "10.242.0.0/16"
+}
+
+variable "subnet_bits" {
+  type    = "string"
+  default = "8"
+}
+
+variable "subnet_prv_offset" {
+  type    = "string"
+  default = "8"
+}
+
+variable "vpc_name" {
+  type    = "string"
+  default = "Salt"
+}
+
+variable "vpc_short_name" {
+  type    = "string"
+  default = "Salt"
+}
--- saltstack/variables.tf
+++ saltstack/variables.tf
@@ -1,15 +1,48 @@
-variable "region" { default = "eu-west-1" }
-variable "application" { default = "saltstack" }
-variable "owner" { default = "d2si" }
-variable "state_bucket" {}
-variable "state_key" {}
-variable "trusted_networks" { default = "0.0.0.0/0" }
-variable "key_name" {}
+variable "region" { 
+    type = "string"
+    default = "eu-west-1"
+}
+variable "application" {
+    type = "string"
+    default = "saltstack"
+}
+variable "owner" {
+    type = "string"
+    default = "d2si" 
+}
+variable "state_bucket" {
+    type = "string"
+}
+variable "state_key" {
+    type = "string"
+}
+variable "trusted_networks" {
+    type = "list"
+    default = [ "0.0.0.0/0" ]
+}
+variable "key_name" {
+    type = "string"
+}
 
-variable "master_instance_type" { default = "t2.micro" }
-variable "minion_instance_type" { default = "t2.micro" }
+variable "master_instance_type" {
+    type = "string"
+    default = "t2.micro" 
+}
+variable "minion_instance_type" {
+    type = "string"
+    default = "t2.micro"
+}
 
-variable "minions_asg_min_size" { default = "1"}
-variable "minions_asg_max_size" { default = "4" }
-variable "minions_asg_desired" { default = "2"}
+variable "minions_asg_min_size" { 
+    type = "string"
+    default = "1"
+}
+variable "minions_asg_max_size" {
+    type = "string"
+    default = "4"
+}
+variable "minions_asg_desired" {
+    type = "string"
+    default = "2"
+}
 

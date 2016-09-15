fb6e743a6cd5389dc22991865566ba6b6dd1f533
Laurent Bernaille <laurent.bernaille@gmail.com>
2016-09-15 21:55:24 +0200

    Separate minions and master. Provide better sg granularity. Use variable for AMI

--- saltstack/saltstack.tf
+++ saltstack/saltstack.tf
@@ -1,80 +1,141 @@
 data "terraform_remote_state" "common" {
-       backend = "s3"
-       config {
-           bucket = "${var.state_bucket}"
-           key = "${var.state_key}"
-           region = "${var.region}"
-       }
+  backend = "s3"
+
+  config {
+    bucket = "${var.state_bucket}"
+    key    = "${var.state_key}"
+    region = "${var.region}"
+  }
 }
 
 provider "aws" {
-    region = "${var.region}"
+  region = "${var.region}"
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "salt" {
   most_recent = true
+
   filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*" ]
+    name   = "name"
+    values = "${list(var.ami_basenames["${var.salt_distrib}"])}"
   }
+
   filter {
-    name = "virtualization-type"
+    name   = "virtualization-type"
     values = ["hvm"]
   }
+
   owners = ["099720109477"] # Canonical
 }
 
-module "saltservers" {
-    source = "../modules/instances"
-    name = ["saltmaster", "minion1"]
+module "saltmaster" {
+  source = "../modules/instances"
+  name   = ["salt"]
 
-    ami_id = "${data.aws_ami.ubuntu.id}"
-    type = "${var.master_instance_type}"
-    key = "${var.key_name}"
-    subnet = "${data.terraform_remote_state.common.public_subnets}"
-    security_groups = ["${aws_security_group.sg_salt.id}"]
+  ami_id          = "${data.aws_ami.salt.id}"
+  type            = "${var.master_instance_type}"
+  key             = "${var.key_name}"
+  subnet          = "${data.terraform_remote_state.common.public_subnets}"
+  security_groups = ["${aws_security_group.sg_admin.id}", "${aws_security_group.sg_saltmaster.id}"]
 
-    user_data = "${file("scripts/master-bootstrap.sh")}"
+  user_data = "${file("scripts/master-bootstrap.sh")}"
 
-    private_zone_id = "${data.terraform_remote_state.common.private_host_zone}"
-    reverse_zone_id = "${data.terraform_remote_state.common.private_host_zone_reverse}"
-    domain_name = "${data.terraform_remote_state.common.private_domain_name}"
+  private_zone_id = "${data.terraform_remote_state.common.private_host_zone}"
+  reverse_zone_id = "${data.terraform_remote_state.common.private_host_zone_reverse}"
+  domain_name     = "${data.terraform_remote_state.common.private_domain_name}"
 }
 
-# Security group for salt instance
-resource "aws_security_group" "sg_salt" {
-    name = "sg_salt"
-    vpc_id = "${data.terraform_remote_state.common.vpc_id}"
+module "saltminions" {
+  source = "../modules/instances"
+  name   = ["minion1", "minion2"]
 
-    # SSH from trustednetworks
-    ingress {
-      from_port = "22"
-      to_port = "22"
-      protocol = "tcp"
-      cidr_blocks = [ "${var.trusted_networks}" ]
-    }
+  ami_id          = "${data.aws_ami.salt.id}"
+  type            = "${var.minion_instance_type}"
+  key             = "${var.key_name}"
+  subnet          = "${data.terraform_remote_state.common.public_subnets}"
+  security_groups = ["${aws_security_group.sg_admin.id}", "${aws_security_group.sg_minion.id}"]
 
-    # saltstack ports (zeroMQ)
-    ingress {
-      from_port = "4505"
-      to_port = "4506"
-      protocol = "tcp"
-      cidr_blocks = [ "${data.terraform_remote_state.common.private_subnets_cidr_block}" ]
-    }
+  user_data = "${file("scripts/minion-bootstrap.sh")}"
 
-    # Goes Anywhere with any protocol
-    egress {
-      from_port = "0"
-      to_port = "0"
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags { Name = "${var.application}" }
+  private_zone_id = "${data.terraform_remote_state.common.private_host_zone}"
+  reverse_zone_id = "${data.terraform_remote_state.common.private_host_zone_reverse}"
+  domain_name     = "${data.terraform_remote_state.common.private_domain_name}"
 }
 
-output  "saltservers_private_dns"            {  value  =  "${module.saltservers.private_dns}" }
-output  "saltservers_private_ip"            {  value  =  "${module.saltservers.private_ip}" }
-output  "saltservers_public_ip"            {  value  =  "${module.saltservers.public_ip}" }
-output  "minions_asg"                     {  value  =  "${aws_autoscaling_group.asg_minions.name}" }
-output  "application"                     {  value  =  "${var.application}" }
-output  "owner"                           {  value  =  "${var.owner}" }
+# Security group for server administration
+resource "aws_security_group" "sg_admin" {
+  name   = "sg_admin"
+  vpc_id = "${data.terraform_remote_state.common.vpc_id}"
+
+  # SSH from trustednetworks
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["${var.trusted_networks}"]
+  }
+}
+
+# Security group for server administration
+resource "aws_security_group" "sg_minion" {
+  name   = "sg_minion"
+  vpc_id = "${data.terraform_remote_state.common.vpc_id}"
+
+  # Goes Anywhere with any protocol
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security group for salt master
+resource "aws_security_group" "sg_saltmaster" {
+  name   = "sg_saltmaster"
+  vpc_id = "${data.terraform_remote_state.common.vpc_id}"
+
+  # saltstack ports (zeroMQ)
+  ingress {
+    from_port       = "4505"
+    to_port         = "4506"
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.sg_minion.id}"]
+  }
+
+  # Goes Anywhere with any protocol
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.application}"
+  }
+}
+
+output "saltmaster_private_dns" {
+  value = "${module.saltmaster.private_dns}"
+}
+
+output "saltmaster_private_ip" {
+  value = "${module.saltmaster.private_ip}"
+}
+
+output "saltmaster_public_ip" {
+  value = "${module.saltmaster.public_ip}"
+}
+
+output "saltminions_private_dns" {
+  value = "${module.saltminions.private_dns}"
+}
+
+output "saltminions_private_ip" {
+  value = "${module.saltminions.private_ip}"
+}
+
+output "saltminions_public_ip" {
+  value = "${module.saltminions.public_ip}"
+}
